ad4ca67fbd08d6695c8c7da20a7d6d1d34728705
Rob Froetscher <rfroetscher@lumoslabs.com>
2016-04-18 17:18:39 -0700

    clean up a few loose ends

--- spec/job_metadata_spec.rb
+++ spec/job_metadata_spec.rb
@@ -4,8 +4,4 @@ describe JobMetadata do
   it 'has a version number' do
     expect(JobMetadata::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
